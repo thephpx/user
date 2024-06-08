791c863a9fabf54f6f506b2e415cf44e50eb5ab5
Faisal Ahmed <thephpx@gmail.com>
2024-06-08 14:05:18 +0600

    moved user model

---
+++ src/Models/User.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Thephpx\User\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
+use App\Models\User as BaseUser;
+use Spatie\Permission\Traits\HasRoles;
+
+class User extends BaseUser
+{
+    use HasFactory, Notifiable, HasRoles;
+}
