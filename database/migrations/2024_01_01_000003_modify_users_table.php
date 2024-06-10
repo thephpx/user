d6e19e919a7541e665980c7a4f1fcc6eb07477a1
Faisal Ahmed <thephpx@gmail.com>
2024-06-10 16:52:36 +0600

    added migration file

---
+++ database/migrations/2024_01_01_000003_modify_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Laravel\Fortify\Fortify;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('first_name')
+                ->after('name')
+                ->nullable();
+
+            $table->string('last_name')
+                ->after('first_name')
+                ->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn([
+                'first_name',
+                'last_name',
+            ]);
+        });
+    }
+};
