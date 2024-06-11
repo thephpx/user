7481c16103c0f74a5419f0b234a51511f5ccd78b
Faisal Ahmed <thephpx@gmail.com>
2024-06-11 22:04:08 +0600

    role crud controllers

---
+++ src/Http/Controllers/Role/Create.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Thephpx\User\Http\Controllers\Role;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request;
+use App\Http\Controllers\AdminController;
+
+class Create extends AdminController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+        
+    public function __invoke(Request $request)
+    {
+        $data = $this->data;
+
+        if($request->isMethod('post'))
+        {
+            $request->validate(
+                ['name'=>'required']
+            );
+
+            $record = $request->only(['name']);
+
+            \Facades\Thephpx\User\Models\Role::create($record);
+        }
+
+        return view('User::tablerui.role.create', compact('data'));
+    }
+}
---
+++ src/Http/Controllers/Role/Index.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Thephpx\User\Http\Controllers\Role;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request;
+use App\Http\Controllers\AdminController;
+
+class Index extends AdminController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+        
+    public function __invoke(Request $request)
+    {
+        $data = $this->data;
+
+        $data['rows'] = \Facades\Thephpx\User\Models\Role::paginate(10);
+
+        return view('User::tablerui.role.index', compact('data'));
+    }
+}
