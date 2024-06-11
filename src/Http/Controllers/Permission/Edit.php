15cc7caacd8fe49984ada511a23cdb548c3442fa
Faisal Ahmed <thephpx@gmail.com>
2024-06-11 22:03:51 +0600

    permission crud controllers

---
+++ src/Http/Controllers/Permission/Edit.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Thephpx\User\Http\Controllers\Permission;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request;
+use App\Http\Controllers\AdminController;
+
+class Edit extends AdminController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+        
+    public function __invoke(Request $request, \Thephpx\User\Models\Permission $permission)
+    {
+        $data = $this->data;
+
+        if($request->isMethod('put'))
+        {
+            $request->validate(
+                ['name'=>'required']
+            );
+            
+            $record = $request->only(['name']);
+
+            $permission->fill($record)->save();
+            
+        }
+
+        return view('User::tablerui.permission.edit', compact('data'));
+    }
+}
---
+++ src/Http/Controllers/Permission/Remove.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Thephpx\User\Http\Controllers\Permission;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request;
+use App\Http\Controllers\AdminController;
+
+class Remove extends AdminController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+        
+    public function __invoke(Request $request, \Thephpx\User\Models\Permission $permission)
+    {
+        $data = $this->data;
+
+        if($request->isMethod('delete'))
+        {
+            
+        }
+
+    }
+}
