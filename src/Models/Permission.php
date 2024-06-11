17f78a7836f9bc3d2dbfece4da12da196c2ac7cb
Faisal Ahmed <thephpx@gmail.com>
2024-06-11 22:04:21 +0600

    role and permission models

---
+++ src/Models/Permission.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Thephpx\User\Models;
+
+use Spatie\Permission\Models\Permission as BasePermission;
+
+class Permission extends BasePermission
+{
+    
+}
---
+++ src/Models/Role.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Thephpx\User\Models;
+
+use Spatie\Permission\Models\Role as BaseRole;
+
+class Role extends BaseRole
+{
+
+}
