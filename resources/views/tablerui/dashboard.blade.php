f51d671e45bc1f117f67d58d47761ebba9eb8275
Faisal Ahmed <thephpx@gmail.com>
2024-06-12 16:41:44 +0600

    updated default dashboard view file

--- resources/views/tablerui/dashboard.blade.php
+++ resources/views/tablerui/dashboard.blade.php
@@ -1,4 +1,19 @@
 @extends("Tablerui::layouts.master")
-@section('content')     
-<h1>Dashboard</h1>
+@section('content')   
+<div class="page-header d-print-none">
+    <div class="container-xl">
+        <div class="row g-2 align-items-center">
+            <div class="col">
+                <h2 class="page-title">
+                    Dashboard
+                </h2>
+            </div>
+        </div>
+    </div>
+    <div class="page-body">
+        <div class="container-xl">
+            <!-- Content here -->
+        </div>
+    </div>
+</div>
 @endsection
