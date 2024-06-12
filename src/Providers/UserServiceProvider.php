5ec0a2b55c1492170e6f62b44324183be812aafe
Faisal Ahmed <thephpx@gmail.com>
2024-06-12 23:22:35 +0600

    updated config and incorporated into service provider

--- src/Providers/UserServiceProvider.php
+++ src/Providers/UserServiceProvider.php
@@ -24,10 +24,38 @@ class UserServiceProvider extends ServiceProvider
     # Load migration tables
     $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
 
+    # Load config
+    $this->mergeConfigFrom(__DIR__.'/../../config/config.php', 'User');
+
     # Publish assets
     $this->publishes([
       __DIR__.'/../../config/fortify.php' => config_path('fortify.php'),
       __DIR__.'/../../config/permission.php' => config_path('permission.php'),
     ], 'config');
+
+    # Register package
+    $packages = \Illuminate\Support\Facades\Session::get('packages');
+    if(empty($packages)){
+      $packages = [];
+    }
+
+    $packages[] = 'thephpx/user';
+
+    \Illuminate\Support\Facades\Session::put('packages', $packages);
+
+    # Register navigation items
+    $navigations = \Illuminate\Support\Facades\Session::get('navigations');
+    if(empty($navigations)){
+      $navigations = [];
+    }
+
+    $navigations[] = ['type'=>'route','label'=>'Dashboard','link'=>'dashboard.index','icon'=>'ti-home'];
+    $navigations[] = ['type'=>'dropdown','label'=>'User Management','link'=>'javascript:;','icon'=>'ti-users-group','items'=>[
+      ['type'=>'route','label'=>'Users','link'=>'user.index','icon'=>'ti-users-group'],
+      ['type'=>'route','label'=>'Roles','link'=>'role.index','icon'=>'ti-circles'],
+      ['type'=>'route','label'=>'Permissions','link'=>'permission.index','icon'=>'ti-key']
+    ]];
+
+    \Illuminate\Support\Facades\Session::put('navigations', $navigations);
   }
 }
